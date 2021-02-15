1a3a672e34577b13f3a2de93a0a68ca1bb18df8d
Paul Gangneux <paul.v.gangneux@gmail.com>
2021-02-15 21:34:14 +0100

    début de création de Player

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,19 @@
 package model;
+import view.MainView;
 
 public class Player {
+    MainView view;
+    Game game;
+
+    public Player(MainView view) {
+
+    }
+
+    // ---------------------------------
+    //  fonctions qui modifient le jeu :
+    // ---------------------------------
+
+    // ---------------------------------
+    //  fonctions qui modifient la vue :
+    // ---------------------------------
 }
