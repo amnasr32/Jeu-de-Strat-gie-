120b0aefa1f9d15cecf8f9cb0638de05aa6ff9a2
Paul Gangneux <paul.v.gangneux@gmail.com>
2021-02-17 20:51:03 +0100

    ajout de loadLevel

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -6,12 +6,18 @@ public class Player {
     Game game;
 
     public Player(MainView view) {
-
+        this.view=view;
+        game=null;
     }
 
     // ---------------------------------
     //  fonctions qui modifient le jeu :
     // ---------------------------------
+
+    public void loadLevel() { //todo : permettre de charger une grille
+        Grid grid = new Grid(10, 15);
+        game = new Game(grid, this, null);
+    }
 
     // ---------------------------------
     //  fonctions qui modifient la vue :
